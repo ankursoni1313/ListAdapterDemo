42bfdf56940ac35cdf4ea7b02d9f28c2670eba92
ankursoni1313 <techtrick13@gmail.com>
2017-04-09 19:43:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ankursoni/listadapterdemo/ItemForMenu.java
@@ -0,0 +1,18 @@
+package com.example.ankursoni.listadapterdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by Ankur soni on 4/7/2017.
+ */
+
+public class ItemForMenu extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.itemfor_menu);
+
+    }
+}
---
+++ app/src/main/java/com/example/ankursoni/listadapterdemo/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.ankursoni.listadapterdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+    String [] listdata={"apple","mango","banana","grapes","pich","pineapple","lemon","apple","mango","banana","grapes","pich","pineapple","lemon","apple","mango","banana","grapes","pich","pineapple","lemon"};
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       listView=(ListView)findViewById(R.id.listviewdemo);
+        ArrayAdapter arrayAdapter=new ArrayAdapter<String>(this,R.layout.item_holder,listdata);
+        listView.setAdapter(arrayAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.primary_menu,menu);
+        return true;
+    }
+    public void loadactivity()
+    {
+        Intent intent=new Intent(MainActivity.this,ItemForMenu.class);
+        startActivity(intent);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.primarybar:
+                loadactivity();
+            case R.id.secondmenuitem:
+                loadactivity();
+            case R.id.subdemo1:
+                loadactivity();
+                default:
+                    return true;
+        }
+    }
+}
