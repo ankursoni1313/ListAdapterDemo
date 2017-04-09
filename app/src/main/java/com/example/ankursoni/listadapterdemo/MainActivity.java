4e321f6d6043115fb57b7a35351c36516b48a3b5
ankursoni1313 <techtrick13@gmail.com>
2017-04-09 20:26:43 +0530

    up

--- app/src/main/java/com/example/ankursoni/listadapterdemo/MainActivity.java
+++ app/src/main/java/com/example/ankursoni/listadapterdemo/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.ListView;
 import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-    String [] listdata={"apple","mango","banana","grapes","pich","pineapple","lemon","apple","mango","banana","grapes","pich","pineapple","lemon","apple","mango","banana","grapes","pich","pineapple","lemon"};
+    String [] listdata={"lemon","apple","mango","banana","grapes","pich","pineapple","lemon"};
     ListView listView;
 
     @Override
